ba33f26dfd81264413e8d335d924a0387aaf23ab
Iestyn Stears <iestynstears@yahoo.co.uk>
2022-06-08 07:42:49 +0100

    inital prototype for event classes

---
+++ liveengine_core/event.py
@@ -0,0 +1,19 @@
+class Event:
+    def __init__(self, **kwargs):
+        pass
+    
+    def trigger(self):
+        pass
+    
+    def set_content(self, content):
+        pass
+    
+    def on_receive(self):
+        pass
+    
+class EventManager:
+    def __init__(self, **kwargs):
+        pass
+    
+    def register_event(self, event_type, on_event):
+        pass
